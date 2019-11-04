9e4bd6786a58da2ae92ba7ca15400d9755abd59a
kavitha <kavithasubbiah@hotmail.com>
2019-11-04 06:09:18 -0500

    first commit

---
+++ src/main/java/com/kav/js/springbootone/SpringbootOneApplication.java
@@ -0,0 +1,13 @@
+package com.kav.js.springbootone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kav/js/springbootone/SpringbootOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kav.js.springbootone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
